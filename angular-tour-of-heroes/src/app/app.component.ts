990032712f39f6900d93e1f3ef798ec54e8048db
Vinicius Jose Schmidt <vinicius.schmidt@cwi.com.br>
2017-03-13 15:54:07 -0300

    primeiro commit

---
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+import { Hero } from './hero';
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
+@Component({
+		selector: 'my-app',
+		template: `
+			<h1>{{title}}</h1>
+			<h2>My Heroes</h2>
+			<ul class="heroes">
+				<li *ngFor="let hero of heroes"
+					[class.selected]="hero === selectedHero"
+					(click)="onSelect(hero)">
+					<span class="badge">{{hero.id}}</span> {{hero.name}}
+				</li>
+			</ul>
+			<my-hero-detail [hero]="selectedHero"></my-hero-detail>
+		`,
+})
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+  heroes = HEROES;
+  selectedHero: Hero;
+	
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
+
+
