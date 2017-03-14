f1783227fa139a608cfb2f170c57e845dc073c20
Vinicius Jose Schmidt <vinicius.schmidt@cwi.com.br>
2017-03-14 09:15:25 -0300

    estrutura inicial do dashboard

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,26 +1,45 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
+import { DashboardComponent }  from './dashboard.component'; 
 import { HeroService }         from './hero.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent,
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      }
+    ])
   ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
+    DashboardComponent,
   ],
   providers: [
     HeroService,
   ],
   bootstrap: [ AppComponent ]
 })
+
 export class AppModule {
 }
